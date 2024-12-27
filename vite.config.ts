7367218be15bfa446a68c242e458dda4a6670111
colinscz <19342760+colinscz@users.noreply.github.com>
2024-12-27 15:37:23 +0100

    chore: fix last dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import path from 'node:path'
+import { fileURLToPath, URL } from 'node:url'
+
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools(),
+  ],
   resolve: {
     alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
